82c8f326d152f3a30bf70c3654211d2fe160ee79
ArtAulas <carelliarthur@gmail.com>
2026-05-11 22:46:05 -0300

    Fix: Correção no nodemailer

--- mail.js
+++ mail.js
@@ -2,6 +2,9 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
